1f9c8ce9d65b71d827b5302bfc015634a84d542e
karenjho <hokarenj@gmail.com>
2016-01-14 13:15:52 -0500

    Define initialize method. Can setx-coordinate, y-coordinate, and direction when creating new Rover.

--- mars_rover.rb
+++ mars_rover.rb
@@ -2,4 +2,10 @@
 class Rover
   # Rover class has three attributes: x-coordinate, y-coordinate, direction
   attr_accessor :x_coordinate, :y_coordinate, :direction
+
+  def initialize(x_coordinate = 0, y_coordinate = 0, direction = "N")
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction = direction
+  end
 end
