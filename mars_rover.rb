809e729d61f7ea2c371e5538ae128092a652de77
karenjho <hokarenj@gmail.com>
2016-01-14 13:30:58 -0500

    Ask user for plateau size

--- mars_rover.rb
+++ mars_rover.rb
@@ -8,4 +8,10 @@ class Rover
     @y_coordinate = y_coordinate
     @direction = direction
   end
+
+  def read_instruction
+    puts "Please provide a plateau size."
+    puts "Format: W L. W = width of plateau. L = height of plateau."
+    plateau_size = gets.chomp.split(" ")
+  end
 end
