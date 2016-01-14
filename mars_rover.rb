8d5c57091c06e03d8cdf3b91eeda077eaf01ef3c
karenjho <hokarenj@gmail.com>
2016-01-14 13:36:22 -0500

    Ask user for a series of move and turn instructions

--- mars_rover.rb
+++ mars_rover.rb
@@ -10,12 +10,16 @@ class Rover
   end
 
   def read_instruction
-    puts "Please provide a plateau size."
-    puts "Format: W L. W = width of plateau. L = height of plateau."
+    puts "Please provide a plateau size in this format: W L"
+    puts "W = width of plateau. L = height of plateau."
     plateau_size = gets.chomp.split(" ")
 
-    puts "Please provide a starting position for your rover."
-    puts "Format: X Y D. X = starting x-coordinate. Y = starting y-coordinate. D = starting direction."
+    puts "Please provide a starting position for the rover in this format: X Y D"
+    puts "X = starting x-coordinate. Y = starting y-coordinate. D = starting direction."
     starting_position = gets.chomp.split(" ")
+
+    puts "Please provide a series of move and turn instructions."
+    puts "L = turn left. R = turn right. M = move 1 space. Do not include spaces."
+    move_turn_instructions = get.chomp.split("")
   end
 end
