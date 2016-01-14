e185e829494f787bb35ed85aa540cf8547178d60
karenjho <hokarenj@gmail.com>
2016-01-14 13:59:26 -0500

    Add turn method for Rover class

--- mars_rover.rb
+++ mars_rover.rb
@@ -14,12 +14,47 @@ class Rover
     puts "W = width of plateau. L = height of plateau."
     plateau_size = gets.chomp.split(" ")
 
-    puts "Please provide a starting position for the rover in this format: X Y D"
+    puts "Please provide a starting position for the first rover in this format: X Y D"
     puts "X = starting x-coordinate. Y = starting y-coordinate. D = starting direction."
-    starting_position = gets.chomp.split(" ")
+    r1_starting_position = gets.chomp.split(" ")
 
-    puts "Please provide a series of move and turn instructions."
+    puts "Please provide a series of move and turn instructions for the first rover."
     puts "L = turn left. R = turn right. M = move 1 space. Do not include spaces."
-    move_turn_instructions = get.chomp.split("")
+    r1_move_turn_instructions = get.chomp.split("")
+
+    puts "Please provide a starting position for the second rover in this format: X Y D"
+    puts "X = starting x-coordinate. Y = starting y-coordinate. D = starting direction."
+    r2_starting_position = gets.chomp.split(" ")
+
+    puts "Please provide a series of move and turn instructions for the second rover."
+    puts "L = turn left. R = turn right. M = move 1 space. Do not include spaces."
+    r2_move_turn_instructions = get.chomp.split("")
   end
+
+  def move
+    case direction
+      when "N"
+        @y_coordinate + 1
+      when "S"
+        @y_coordinate - 1
+      when "E"
+        @x_coordinate + 1
+      when "W"
+        @x_coordinate - 1
+    end
+  end
+
+  def turn
+    case right
+      when "N"
+        @direction = "E"
+      when "S"
+        @direction = "W"
+      when "E"
+        @direction = "S"
+      when "W"
+        @direction = "N"
+    end
+  end
+  
 end
