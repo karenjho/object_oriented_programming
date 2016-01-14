3de0b72762f540ec7702a365e7c6c6eb50f583b2
karenjho <hokarenj@gmail.com>
2016-01-14 12:35:37 -0500

    Add new methods. Student can now learn. Instructor can now teach.

--- people.rb
+++ people.rb
@@ -1,6 +1,12 @@
 # create two classes: Student and Instructor
 class Student
+  def learn # add learn method to Student
+    puts "I get it!"
+  end
 end
 
 class Instructor
+  def teach # add teach method to Instructor
+    "Everything in Ruby is an Object."
+  end
 end
