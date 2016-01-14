f2cc60dd522295debd940166041cbd3e959673ea
karenjho <hokarenj@gmail.com>
2016-01-14 12:47:18 -0500

    Make Person class. Student and Instructor are Person, too.

--- people.rb
+++ people.rb
@@ -1,11 +1,15 @@
 # create two classes: Student and Instructor
-class Student
+class Person # create Person class
+  attr_accessor :name
+end
+
+class Student < Person # add Student class to Person class
   def learn # add learn method to Student
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor # add Instructor class to Person class
   def teach # add teach method to Instructor
     "Everything in Ruby is an Object."
   end
