ea756899110119d74d52d435746cef8245ae6a50
karenjho <hokarenj@gmail.com>
2016-01-14 15:44:34 -0500

    Add tax rate and import duty for Good class

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -1,11 +1,13 @@
 class Good
   attr_accessor :name, :quantity, :unit_price
-  attr_reader :tax_rate
+  attr_reader :tax_rate, :import_duty
 
   def initialize(name, quantity=1, unit_price)
     @name = name
     @quantity = quantity
     @unit_price = unit_price
+    @tax_rate = 0.10
+    @import_duty = 0.05
   end
-
+  
 end
